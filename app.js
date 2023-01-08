816c793572ceac7e4a114e6febe7332b22e522f2
Priyal Jain <94099575+priyalj52@users.noreply.github.com>
2023-01-08 19:35:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ app.post("/",function(req,res){
 
   };
   var jsonData=JSON.stringify(data);
-  const url="https://"+{dc}+".api.mailchimp.com/3.0/lists/"+{list-id};
+  const url="https://"+{process.env.dc}+".api.mailchimp.com/3.0/lists/"+{process.env.list-id};
   const options={
     method:"POST",
-    auth:"priyal01:"+{api-key}
+    auth:"priyal01:"+{process.env.api-key}
   };
  
     const request=https.request(url,options,function(response)
